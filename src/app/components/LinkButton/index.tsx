513d9a2e4729e752a5b3c4df1b112222f5b49f6d
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2021-11-04 19:26:22 -0600

    Start dark mode design

--- src/app/components/LinkButton/index.tsx
+++ src/app/components/LinkButton/index.tsx
@@ -12,11 +12,13 @@ type Props = {
 export default function LinkButton({ to, title, description, logo }: Props) {
   return (
     <Link to={to} className="block">
-      <div className="p-4 bg-white flex justify-between items-center shadow overflow-hidden border-b border-gray-200 rounded-lg hover:bg-gray-50 transition duration-200">
+      <div className="p-4 bg-white dark:bg-gray-800 flex justify-between items-center shadow overflow-hidden border-b border-gray-200 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700 transition duration-200">
         <div>
-          <span className="block text-lg">{title}</span>
+          <span className="block dark:text-white text-lg">{title}</span>
           {description && (
-            <span className="text-sm text-gray-500">{description}</span>
+            <span className="text-sm text-gray-500 dark:text-gray-300">
+              {description}
+            </span>
           )}
         </div>
         <div>
@@ -27,7 +29,10 @@ export default function LinkButton({ to, title, description, logo }: Props) {
             height="75em"
             className="inline rounded-full mr-3"
           />
-          <ChevronRightIcon className="h-5 w-5 inline" aria-hidden="true" />
+          <ChevronRightIcon
+            className="h-5 w-5 inline dark:text-white"
+            aria-hidden="true"
+          />
         </div>
       </div>
     </Link>
